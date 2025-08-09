45abff0b44cf0f020dfae734aa458f6d99744819
kgxkaran <karangera025@gmail.com>
2025-08-09 23:42:47 +0530

    Update SecondSmallestLargest.cpp

--- Arrays/Easy/SecondSmallestLargest.cpp
+++ Arrays/Easy/SecondSmallestLargest.cpp
@@ -25,3 +25,28 @@ public:
         return second_large;
     }
 };
+
+
+3 . 
+
+  int secondLargest(int arr[],int n)
+{
+	if(n<2)
+	return -1;
+    int large=INT_MIN,second_large=INT_MIN;
+    int i;
+    for (i = 0; i < n; i++) 
+    {
+        if (arr[i] > large) 
+        {
+            second_large = large;
+            large = arr[i];
+        }
+ 
+        else if (arr[i] > second_large && arr[i] != large) 
+        {
+            second_large = arr[i];
+        }
+    }
+    return second_large;                
+}
