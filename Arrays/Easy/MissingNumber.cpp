7844174b31b7a227f14fe53773b6925a2d97c2a0
kgxkaran <karangera025@gmail.com>
2025-08-16 16:25:36 +0530

    Update MissingNumber.cpp

--- Arrays/Easy/MissingNumber.cpp
+++ Arrays/Easy/MissingNumber.cpp
@@ -37,3 +37,19 @@ public:
         return total-sum;
     }
 };
+
+Approach 3 :-  using properties of XOR
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int ans = n;
+        for (int i = 0; i < n; i++) {
+            ans ^= i ^ nums[i];
+        }
+        return ans;
+    }
+};
+
+  
